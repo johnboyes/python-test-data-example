fd1248915577336bbf008088c751aa9fc1ef838d
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 13:54:51 +0100

    Very basic InputRequest class

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./hello_world.py" ]
+CMD [ "python", "./input_request.py" ]
---
+++ input_request.py
@@ -0,0 +1,8 @@
+class InputRequest:
+    def __str__(self):
+        return "This is an example input request"
+
+
+input_request = InputRequest()
+
+print(input_request)
