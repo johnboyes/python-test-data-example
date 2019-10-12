3a3a1ddaaa924f6f6304aaa0ade48f8563912891
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 14:05:39 +0100

    Use named argument alongside positional argument

--- input_request.py
+++ input_request.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 class InputRequest:
     def __str__(self):
-        return "This is an example input request. Start time: {:%Y-%m-%d %H:%M}".format(
-            datetime(2001, 2, 3, 4, 5)
+        return "Start time: {start_time} End time: {:%Y-%m-%d}".format(
+            datetime(2001, 2, 3, 4, 5), start_time=datetime(2001, 2, 3, 4, 5)
         )
 
 
