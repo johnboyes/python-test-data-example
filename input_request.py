f9ee3d48cbe8a33b134e894348de34191f9ec96b
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 15:26:01 +0100

    Make keyword arguments mandatory

--- input_request.py
+++ input_request.py
@@ -4,7 +4,7 @@ from request_date_utils import request_date
 class InputRequest:
     def __init__(self):
         self.start_time = request_date()
-        self.end_time = request_date("{:%Y-%m-%d}", 1)
+        self.end_time = request_date(format="{:%Y-%m-%d}", days=1)
 
     def __str__(self):
         return f"Start time: {self.start_time} End time: {self.end_time}"
--- request_date_utils.py
+++ request_date_utils.py
@@ -2,5 +2,5 @@ from datetime import datetime
 from datetime import timedelta
 
 
-def request_date(format="{:%Y-%m-%d %H:%M}", days=0):
+def request_date(*, format="{:%Y-%m-%d %H:%M}", days=0):
     return format.format(datetime.now() + timedelta(days=days))
