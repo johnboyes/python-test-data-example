ac78a8b736d22b98323cee9c65957bf10debe07e
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 13:59:18 +0100

    Basic formatting of date time

--- input_request.py
+++ input_request.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+
 class InputRequest:
     def __str__(self):
-        return "This is an example input request"
+        return "This is an example input request. Start time: {:%Y-%m-%d %H:%M}".format(
+            datetime(2001, 2, 3, 4, 5)
+        )
 
 
 input_request = InputRequest()
