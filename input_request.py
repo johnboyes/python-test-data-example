bb588df3c6545158bf1b527bb1ab5c0d33df3183
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 14:48:36 +0100

    Refactor: use init method

--- input_request.py
+++ input_request.py
@@ -2,14 +2,12 @@ from datetime import datetime
 
 
 class InputRequest:
-    def start_time(self):
-        return "{:%Y-%m-%d %H:%M}".format(datetime(2001, 2, 3, 4, 5))
-
-    def end_time(self):
-        return "{:%Y-%m-%d}".format(datetime(2001, 2, 3, 4, 5))
+    def __init__(self):
+        self.start_time = "{:%Y-%m-%d %H:%M}".format(datetime(2001, 2, 3, 4, 5))
+        self.end_time = "{:%Y-%m-%d}".format(datetime(2001, 2, 3, 4, 5))
 
     def __str__(self):
-        return f"Start time: {self.start_time()} End time: {self.end_time()}"
+        return f"Start time: {self.start_time} End time: {self.end_time}"
 
 
 input_request = InputRequest()
