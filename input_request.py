bbfa2d3fef66af9e5c70bc0c4830e8dad54d7628
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 14:40:40 +0100

    Extract instance methods

--- input_request.py
+++ input_request.py
@@ -2,10 +2,14 @@ from datetime import datetime
 
 
 class InputRequest:
+    def start_time(self):
+        return "{:%Y-%m-%d %H:%M}".format(datetime(2001, 2, 3, 4, 5))
+
+    def end_time(self):
+        return "{:%Y-%m-%d}".format(datetime(2001, 2, 3, 4, 5))
+
     def __str__(self):
-        return "Start time: {start_time} End time: {:%Y-%m-%d}".format(
-            datetime(2001, 2, 3, 4, 5), start_time=datetime(2001, 2, 3, 4, 5)
-        )
+        return f"Start time: {self.start_time()} End time: {self.end_time()}"
 
 
 input_request = InputRequest()
