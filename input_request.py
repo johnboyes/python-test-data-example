262e8b22d1371b3be2c5efacc9d4f6a6b4c34f34
John Boyes <john@edinburghwebservices.co.uk>
2019-10-12 14:51:45 +0100

    Use today's date

--- input_request.py
+++ input_request.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 class InputRequest:
     def __init__(self):
-        self.start_time = "{:%Y-%m-%d %H:%M}".format(datetime(2001, 2, 3, 4, 5))
-        self.end_time = "{:%Y-%m-%d}".format(datetime(2001, 2, 3, 4, 5))
+        today = datetime.now()
+        self.start_time = "{:%Y-%m-%d %H:%M}".format(today)
+        self.end_time = "{:%Y-%m-%d}".format(today)
 
     def __str__(self):
         return f"Start time: {self.start_time} End time: {self.end_time}"
